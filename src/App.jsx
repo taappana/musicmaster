0bc344fa32b9d3330ae9c0fe18b75e1d0c31e42f
taappana <herranen.jani@gmail.com>
2017-05-25 21:35:19 +0300

    Created the App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ class App extends Component {
     return (
       <div>
         <div className="App-title">Music Master</div>
+        <div>
+          <input placeholder="Search an artist..."/>
+        </div>
+        <button>Hit Submit</button>
+        <div className="Profile">
+          <div>Artist Picture</div>
+          <div>Artist Name</div>
+        </div>
+        <div className="Gallery">
+          Gallery
+        </div>
       </div>
     )
   }
