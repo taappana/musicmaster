8e03d966a06c5f9eb0e72193ebfab2ae20444870
taappana <herranen.jani@gmail.com>
2017-05-26 20:13:20 +0300

    Build the artist profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css'
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap'
+import Profile from './Profile'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      artist: null
     }
   }
 
@@ -14,7 +16,14 @@ class App extends Component {
     console.log('this.state', this.state);
     const BASE_URL = 'https://api.spotify.com/v1/search?';
     const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-    console.log('FETCH_URL', FETCH_URL);
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];
+      this.setState({artist});
+    });
   }
 
   render() {
@@ -39,10 +48,9 @@ class App extends Component {
             </InputGroup.Addon>
           </InputGroup>
         </FormGroup>
-        <div className="Profile">
-          <div>Artist Picture</div>
-          <div>Artist Name</div>
-        </div>
+        <Profile
+          artist={this.state.artist}
+        />
         <div className="Gallery">
           Gallery
         </div>
